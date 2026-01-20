79d31220a11ddda7f76b62462bc5e7bf471ff26c
Ayush Sharma <ayushsharma25072007@gmail.com>
2026-01-20 16:35:01 +0530

    Just Cause

--- backend/configs/ratelimit.config.js
+++
@@ -1,9 +0,0 @@
-const { rateLimit } = require("express-rate-limit");
-
-const routerRateLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 5,
-  message: { message: "Too many requests, please try again later." },
-});
-
-module.exports = routerRateLimiter;
--- backend/routes/index.route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const google = require("./google/index.route");
-const user = require("./user/index.route");
-
-router.use("/google", google);
-router.use("/users", user);
-
-module.exports = router;
